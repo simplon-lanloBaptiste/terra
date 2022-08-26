a00f618c57e4e040cf66753abaa56761c9a7ed4b
simplon-lanloBaptiste <108000853+simplon-lanloBaptiste@users.noreply.github.com>
2022-08-26 16:36:35 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
 
 
 resource "azurerm_subnet" "myterraformsubnet" {
-  name                 = var.prefix
+  name                 = "${var.prefix}_subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
   address_prefixes     = ["10.0.4.0/24"]
