92d2dd13aad998ee4c70c3a824fcf9411b6f4438
simplon-lanloBaptiste <108000853+simplon-lanloBaptiste@users.noreply.github.com>
2022-09-02 15:04:18 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -177,7 +177,7 @@ resource "azurerm_lb" "load-balance" {
   name                = "${var.prefix}_load-blance"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "standar"
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "${var.prefix}ip_public2"
