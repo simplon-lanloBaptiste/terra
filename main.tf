deb3f7e4882410dfc636b42e6b68225115dd6a70
simplon-lanloBaptiste <108000853+simplon-lanloBaptiste@users.noreply.github.com>
2022-09-02 10:24:58 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -202,7 +202,6 @@ resource "azurerm_lb_backend_address_pool_address" "backendpollAdresse" {
 
 resource "azurerm_lb_backend_address_pool_address" "backendpollAdresse2" {
   name                    = "example"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   backend_address_pool_id = data.azurerm_lb_backend_address_pool.backendpoolpanel.id
   virtual_network_id      = data.azurerm_virtual_network.myterraformnetwork.id
@@ -211,7 +210,6 @@ resource "azurerm_lb_backend_address_pool_address" "backendpollAdresse2" {
 
 resource "azurerm_lb_outbound_rule" "outbound_rule_panel" {
   name                    = "${var.prefix}OutboundRule"
-  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   loadbalancer_id         = azurerm_lb.load-balance.id
   protocol                = "Tcp"
